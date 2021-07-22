7b6c2ffb2e1cd2da94fe27da4330259398e86b2a
alastaira <alastair@playful.technology>
2021-07-22 12:52:54 +0100

    Various updates

--- FruitMachineCounter/StarpointElectronicCounter/StarpointElectronicCounter.ino
+++ FruitMachineCounter/StarpointElectronicCounter/StarpointElectronicCounter.ino
@@ -1,15 +1,15 @@
 /*
 See https://www.mikrocontroller.net/attachment/163201/Zaehlwerk_27-080_01__man.pdf
+Also https://forum.arduino.cc/t/how-fast-is-shiftout/369057/2
+Useful info here: https://arduino.stackexchange.com/questions/16348/how-do-you-use-spi-on-an-arduino
  */
 
-// DEFINES
-// Response code values
-#define ACK 61
-#define DATA 60
-#define NAK 62
-
-// INCLUDES
-#include <SPI.h>
+// THE FOLLOWING CODE USES SHIFTIN/SHIFTOUT (doesn't work)  
+// CONSTANTS
+const byte resetPin = A5;
+const byte dataOutPin = 11;
+const byte dataInPin = 12;
+const byte clockPin = 13;
 
 // GLOBALS
 // Array to hold received responses
@@ -18,34 +18,23 @@ byte response[8];
 void setup() {
   Serial.begin(115200);
 
-  pinMode(11, OUTPUT);
-  pinMode(13, OUTPUT);
- 
-  // Start the SPI interface
-  SPI.begin();
+  pinMode(dataOutPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+  pinMode(dataInPin, INPUT);
 
-/*
-  // According to "Communications Interface" (p.8)
-  // Uses SPI Mode 2, MSB, Clock speed between 100Hz - 5kHz
-  SPI.beginTransaction(SPISettings(100, MSBFIRST, SPI_MODE2));
-
-  // Send a command - see "Command Format From Host" (p.10) for general structure
-  // This is "r. <54> Cycle Counter Display" (p.19)
-  // Which should cause counter to flash through all counter values
-  SPI.transfer(0x54); // CMD
-  SPI.transfer(0x00); // ID starts at 00 and increments with each message sent
-  SPI.transfer(0x00); // DCNT - number of databytes following (not including CHKSUM)
-  SPI.transfer(0x54); // CHECKSUM is lower 8 bits of sum of all preceding bytes
-
-  SPI.endTransaction();
+  // If desired, pull reset pin LOW to reset the counter
+  digitalWrite(resetPin, LOW);
+  delay(1000);
+  digitalWrite(resetPin, HIGH);
+  delay(8000);
 
   // Clear the response buffer
-  memset(response, 0, sizeof(response));
-  // Then fill it with 4 byte response
-  for (int i=0; i<4; i++) {
-    response[i] = SPI.transfer(0x00); // Dummy value just to receive SPI response
-  }
-*/  
+  memset(response, 0, sizeof(response));  
+
+  // Set clock line to idle HIGH
+  digitalWrite(clockPin, HIGH);
+
 }
 
 void printBuffer() {
@@ -57,48 +46,150 @@ void printBuffer() {
 }
 
 void loop() {
-  // Set clock line to idle HIGH
-  digitalWrite(13, HIGH);
-
-  // According to "Communications Interface" (p.8)
-  // Uses SPI Mode 2, MSB, Clock speed between 100Hz - 5kHz  
-  SPI.beginTransaction(SPISettings(1000, MSBFIRST, SPI_MODE2));
-
-/*
-  // Send a command - see "Command Format From Host" (p.10) for general structure
-  // This is "r. <21> Request Market Type"
-  // as shown in example on p.29
-  SPI.transfer(0x21); // CMD
-  SPI.transfer(0x01); // ID starts at 00 and increments with each message sent
-  SPI.transfer(0x00); // DCNT - number of databytes following (not including CHKSUM)
-  SPI.transfer(0x22); // CHECKSUM is lower 8 bits of sum of all preceding bytes
-*/
-  // Send a command - see "Command Format From Host" (p.10) for general structure
-  // This is "r. <54> Cycle Counter Display" (p.19)
-  // Which should cause counter to flash through all counter values
-  SPI.transfer(0x54); // CMD
-  SPI.transfer(0x00); // ID starts at 00 and increments with each message sent
-  SPI.transfer(0x00); // DCNT - number of databytes following (not including CHKSUM)
-  SPI.transfer(0x54); // CHECKSUM is lower 8 bits of sum of all preceding bytes
-
-
-
-/*
   // Clear the response buffer
   memset(response, 0, sizeof(response));
-  // Then fill it with 4 byte response
-  for (int i=0; i<4; i++) {
-    response[i] = SPI.transfer(0x00); // Dummy value just to receive SPI response
-  }
-*/
-  // Not sure if this is strictly required - if we're only communicating with a single device,
-  // can probably just open a single transaction and leave it open
-  SPI.endTransaction();
-  // Set clock HIGH again
-  digitalWrite(13, HIGH);
+  
+  // Set clock line to idle HIGH
+  // "the SEC unit uses SPI mode 2.  In this  mode, the clock line (SCLOCK) idle state is HIGH (logic 1)"
+  digitalWrite(clockPin, HIGH);
 
+  // Send a command - see "Command Format From Host" (p.10) for general structure
+  // This is "t. <5C> Self Test" (p.19)
+  customShiftOut(dataOutPin, clockPin, MSBFIRST, 0x5C); // CMD
+  customShiftOut(dataOutPin, clockPin, MSBFIRST, 0x00); // ID starts at 00 and increments with each message sent
+  customShiftOut(dataOutPin, clockPin, MSBFIRST, 0x00); // DCNT - number of databytes following (not including CHKSUM)
+  customShiftOut(dataOutPin, clockPin, MSBFIRST, 0x5C); // CHECKSUM is lower 8 bits of sum of all preceding bytes
+
+  // Then fill the buffer with 4 byte response
+  for (int i=0; i<4; i++) {
+    response[i] = customShiftIn(dataInPin, clockPin, MSBFIRST);
+  }
   // Print the response
   printBuffer();
 
-  delay(1000);  
+  delay(5000);  
 }
+
+
+// Shift functions adapted from the Arduino source code at https://github.com/arduino/ArduinoCore-avr/blob/master/cores/arduino/wiring_shift.c
+// to accommodate data being clocked on the *falling* edge, and at a slower speed 
+void customShiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val) {
+  uint8_t i;
+  for (i = 0; i < 8; i++)  {
+    if (bitOrder == LSBFIRST)
+      digitalWrite(dataPin, !!(val & (1 << i)));
+    else  
+      digitalWrite(dataPin, !!(val & (1 << (7 - i))));
+
+    // 50% duty cycle @ 1KHz
+    digitalWrite(clockPin, LOW); 
+    delayMicroseconds(500);
+    digitalWrite(clockPin, HIGH); 
+    delayMicroseconds(500);   
+  }
+}
+uint8_t customShiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
+  uint8_t value = 0;
+  uint8_t i;
+  for (i = 0; i < 8; ++i) {
+    // 1KHz
+    digitalWrite(clockPin, LOW);
+    if (bitOrder == LSBFIRST)
+      { value |= digitalRead(dataPin) << i; }
+    else
+      { value |= digitalRead(dataPin) << (7 - i); }
+
+   delayMicroseconds(500);
+   digitalWrite(clockPin, HIGH);
+   delayMicroseconds(500);
+  }
+}
+
+
+
+
+/* THE FOLLOWING CODE USES HARDWARE SPI (doesn't work) */
+/*
+// DEFINES
+// Response code values
+#define ACK 61
+#define DATA 60
+#define NAK 62
+
+// CONSTANTS
+const byte resetPin = A5;
+const byte dataOutPin = 11;
+const byte dataInPin = 12;
+const byte clockPin = 13;
+
+// INCLUDES
+#include <SPI.h>
+
+// GLOBALS
+// Array to hold received responses
+byte response[8];
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(clockPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+
+  digitalWrite(resetPin, LOW);
+  delay(1000);
+  digitalWrite(resetPin, HIGH);
+  delay(8000);
+
+  // Clear the response buffer
+  memset(response, 0, sizeof(response));  
+ 
+  // Start the SPI interface
+  SPI.begin();
+  // This shouldn't be necessary because SPISettings should set speed anyway, but let's try it...
+  SPI.setClockDivider(SPI_CLOCK_DIV128);
+
+  // Set clock line to idle HIGH
+  digitalWrite(clockPin, HIGH);
+
+  // According to "Communications Interface" (p.8)
+  // Uses SPI Mode 2, MSB, Clock speed between 100Hz - 5kHz
+  SPI.beginTransaction(SPISettings(1000, MSBFIRST, SPI_MODE2));
+  digitalWrite(clockPin, HIGH);
+  // Send a command - see "Command Format From Host" (p.10) for general structure
+  // This is "t. <5C> Self Test" (p.19)
+  SPI.transfer(0x5C); // CMD
+    digitalWrite(clockPin, HIGH);
+  SPI.transfer(0x00); // ID starts at 00 and increments with each message sent
+    digitalWrite(clockPin, HIGH);
+  SPI.transfer(0x00); // DCNT - number of databytes following (not including CHKSUM)
+    digitalWrite(clockPin, HIGH);
+  SPI.transfer(0x5C); // CHECKSUM is lower 8 bits of sum of all preceding bytes
+  digitalWrite(clockPin, HIGH);
+  SPI.endTransaction();
+
+  // Set clock line to idle HIGH
+  digitalWrite(clockPin, HIGH);
+
+  // Then fill it with 4 byte response
+  for (int i=0; i<4; i++) {
+    // Set clock line to idle HIGH
+    digitalWrite(clockPin, HIGH);
+    response[i] = SPI.transfer(0x00); // Dummy value just to receive SPI response
+    // Set clock line to idle HIGH
+    digitalWrite(clockPin, HIGH);
+  }
+  // Print the response
+  printBuffer();
+}
+
+void printBuffer() {
+  for(int i=0; i<8; i++) {
+    Serial.print(response[i]);
+    if(i<7) Serial.print(",");
+  }
+  Serial.println("");
+}
+
+void loop() {
+}
+*/
--- FruitMachineVFD/FruitMachineVFD.ino
+++ FruitMachineVFD/FruitMachineVFD.ino
@@ -1,10 +1,46 @@
+
+// https://www.fruitemu.co.uk/ib/index.php?app=forums&module=forums&controller=topic&id=17419
+
+// INCLUDES
+#include <SPI.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  // 10 is RESET
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
+  
+  // Start the SPI interface
+  SPI.begin();
+  //SPI.setClockDivider(SPI_CLOCK_DIV128);
+}
+
+void sendCommand(byte command) {
+  digitalWrite(13, HIGH);
+  // Set SPI Mode
+  // "I am pulsing the clock on the display at about 30-40ms." = 25Hz - 33Hz? 
+  SPI.beginTransaction(SPISettings(4000, LSBFIRST, SPI_MODE0));  
+  // "To send the bits out you place the relevant bit value on pin 2 of the port and then provide it a LO clock pulse . That is a HI-LO. "
+  SPI.transfer(command); // Turn on display
+  SPI.endTransaction();
+    digitalWrite(13, HIGH);
+}
+
+void loop() {
+ sendCommand(130); // Turn on screen
+ sendCommand(172); // Test
+ sendCommand(20); // Test
+ delay(1000);
+}
+                              
+// See https://picaxeforum.co.uk/threads/using-spi-serial-vfds-from-fruit-machines-with-picaxe-comprehensive-code-examples.22552/ 
 /*
-See https://picaxeforum.co.uk/threads/using-spi-serial-vfds-from-fruit-machines-with-picaxe-comprehensive-code-examples.22552/ 
-
-// However, have had little luck converting to Arduino - not sure if that's because the hardware is DOA
-
-*/
-
 // INCLUDES
 #include <SPI.h>
 
@@ -17,7 +53,7 @@ void setup() {
   // As the VFD will be the only thing on the bus for now, /SEL can remain low when idle.   
   pinMode(10, OUTPUT);
   digitalWrite(10, LOW);
-
+  
   // Start the SPI interface
   SPI.begin();
 
@@ -26,14 +62,13 @@ void setup() {
 }
 
 void sendCommand(byte command) {
+// See also https://forum.arduino.cc/t/spi-transactions-not-working-with-atmega1280/319194/2
   // "Before sending an command, it (SEL) must then be pulsed high."
   digitalWrite(10, HIGH);
   delayMicroseconds(10);
   digitalWrite(10, LOW);
   SPI.transfer(command); // Turn on display
 }
-
-
 
 void loop() {
  sendCommand(0x60); // Set display length
@@ -45,3 +80,4 @@ void loop() {
 
  delay(100);
 }
+*/
